73f2ea22e8d0b260acb84584c2666b0e90486b4f
TyranRexUA <stanislav.pohrebniakov@onix-systems.com>
2024-08-05 20:54:19 +0300

    finish lesson 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,21 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
 
-export default function Customers() {
-  return <p>Customers Page</p>;
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Customers({ searchParams }: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query)
+
+  return (
+    <CustomersTable customers={customers} />
+  );
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,9 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { ReactNode } from 'react';
 
 export const experimental_ppr = true;
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
